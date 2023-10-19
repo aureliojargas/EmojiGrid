dfe048ab91e57666fedf9bb135fe25f48837c57d
Aurelio Jargas <verde@aurelio.net>
2023-10-19 07:41:33 +0200

    Initial commit: showing a board of cards
    After watching some videos on the MAD Skills series on YouTube about
    compose, I've created this new project and played with if for ~2 hours.
    This is the current state.
    
    As my first toy app, I want to build a memory game using emojis as
    cards, so I can do it without any real image manipulation. Also, my kids
    like emojis, so they can participate choosing the emojis and the card
    colors.
    
    I got the grid example from
    https://alexzh.com/jetpack-compose-building-grids/ but had to adapt it
    (it's from 2 years ago).
    
    At this point:
    
    - There's a board with 16 cards (4x4)
    - There's pairs of different cards using emojis
    - Each pair has a random background color
    - All the cards are showing up with the face up
    - Still no interaction
    
    I'm now studying how to handle clicks, to be able to turn around the
    clicked card.

---
+++ app/src/main/java/com/example/emojigrid/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.emojigrid
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.cardColors
+import androidx.compose.material3.CardDefaults.shape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.emojigrid.ui.theme.EmojiGridTheme
+import kotlin.random.Random
+
+val emojis = mutableListOf("☕️", "🙂", "🥛", "🎉", "📐", "🎯", "🧩", "🥑")
+val found = mutableListOf("")
+val colors = (0..7).map { getRandomColor() }
+
+//    val data = emojis zip colors
+//    val board = (data + data).shuffled()
+val fontSize = 42
+val board = ((0 until emojis.size) + (0 until emojis.size)).shuffled()
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EmojiGridTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    Greeting("Android")
+                    MyPreview(emojis)
+                }
+            }
+        }
+    }
+}
+
+fun getRandomColor() = Color(
+    red = Random.nextInt(0, 255),
+    green = Random.nextInt(0, 255),
+    blue = Random.nextInt(0, 255)
+)
+// also Color.LightGray and so on
+
+
+// https://alexzh.com/jetpack-compose-building-grids/
+//@Preview(showBackground = true)
+@Composable
+fun MyPreview(emojis: MutableList<String>) {
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(4),
+        contentPadding = PaddingValues(8.dp),
+    ) {
+        items(board.size) { item ->
+            Box(
+                modifier = Modifier
+                    .padding(4.dp)
+                    // https://foso.github.io/Jetpack-Compose-Playground/foundation/shape/
+                    .clip(shape = RoundedCornerShape(10.dp))
+                    .background(color = if (emojis[board[item]] in found) Color.White else colors[board[item]])
+                    .clickable {
+                        if (emojis[board[item]] !in found) {
+                            Log.println(Log.INFO, "Emoji", "CLICKED")
+                            Log.println(Log.INFO, "Emoji", found.toString())
+                            found.add(emojis[board[item]])
+                            Log.println(Log.INFO, "Emoji", found.toString())
+//                        Log.d(TAG, "focusRequester.requestFocus()")
+                        }
+                    }
+            ) {
+                Text(
+                    text = emojis[board[item]],
+                    fontSize = fontSize.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = (fontSize * 0.8).dp)
+                )
+            }
+        }
+    }
+}
+
+//            Card(
+//                modifier = Modifier
+//                    .padding(4.dp),
+//                colors = cardColors(
+//                    containerColor = Color(
+//                        red = Random.nextInt(0, 255),
+//                        green = Random.nextInt(0, 255),
+//                        blue = Random.nextInt(0, 255)
+//                    ),
+//                )
